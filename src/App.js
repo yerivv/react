87bd165991c3d7f553cb0e3733447c34a1d476fb
yeri <seo@infi.co.kr>
2023-05-09 16:44:43 +0900

    간단한 연산자

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+//함수형
+const App = () => {
   const name1 = 'YR';
+  const name2 = undefined;
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,9 +20,20 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      {/* 삼항연산자 */}
       <div>
         {name1 === 'YR' ? <div>맞아요</div> : <div>틀려요</div>}
+        {name1 === 'YY' ? <div>맞아요</div> : <div>틀려요</div>}
+      </div>
+      {/* AND 연산자(&&) : And연산자는 a && b 에서 a와 b 둘다 true일 경우에만 식이 성립 */}
+      <div>
+        {name1 === 'YR' && <div>YR 맞아요</div>}
+        {name2 && <div>YR 틀려요</div>}
+      </div>
+      {/* OR 연산자(||) : OR연산자는 a || b 에서 a와 b 둘 중 하나만  true여도 식이 성립 */}
+      <div>
+        {name2 || <div>YR 틀려요</div>}
       </div>
     </div>
   );
